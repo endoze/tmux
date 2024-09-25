1d9669ca0d9225aaa183831d2a8feeaa1bc0de02
Endoze <endoze@users.noreply.github.com>
2024-09-24 20:45:07 -0400

    Fix network script
    In order to ensure the network script works with MacOS 15+, this commit
    updates it to correctly try to get the ssid using the systemprofiler
    command.

--- scripts/network.sh
+++ scripts/network.sh
@@ -2,42 +2,45 @@
 # setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
-HOSTS="google.com github.com example.com"
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $current_dir/utils.sh
 
-get_ssid()
-{
+# set your own hosts so that a wifi is recognised even without internet access
+HOSTS=$(get_tmux_option "@dracula-network-hosts" "google.com github.com example.com")
+
+get_ssid() {
   # Check OS
   case $(uname -s) in
-    Linux)
-      SSID=$(iw dev | sed -nr 's/^\t\tssid (.*)/\1/p')
-      if [ -n "$SSID" ]; then
-        printf '%s' "$SSID"
-      else
-        echo 'Ethernet'
-      fi
-      ;;
+  Linux)
+    SSID=$(iw dev | sed -nr 's/^\t\tssid (.*)/\1/p')
+    if [ -n "$SSID" ]; then
+      printf '%s' "$wifi_label$SSID"
+    else
+      echo "$(get_tmux_option "@dracula-network-ethernet-label" "Ethernet")"
+    fi
+    ;;
 
-    Darwin)
-      if /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep -E ' SSID' | cut -d ':' -f 2 | sed 's/ ^*//g' &> /dev/null; then
-        echo "$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep -E ' SSID' | cut -d ':' -f 2)" | sed 's/ ^*//g'
-      else
-        echo 'Ethernet'
-      fi
-      ;;
+  Darwin)
+    SSID=$(system_profiler SPAirPortDataType | awk '/Current Network Information:/ { getline; print substr($0, 13, (length($0) - 13)); exit }')
+    if [ -n "$SSID" ]; then
+      wifi_label=$(get_tmux_option "@dracula-network-wifi-label" "")
+      echo "$wifi_label$SSID"
+    else
+      echo "$(get_tmux_option "@dracula-network-ethernet-label" "Ethernet")"
+    fi
+    ;;
 
-    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-      # leaving empty - TODO - windows compatability
-      ;;
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
+    # leaving empty - TODO - windows compatability
+    ;;
 
-    *)
-      ;;
+  *) ;;
   esac
 
 }
 
-main()
-{
-  network="Offline"
+main() {
+  network="$(get_tmux_option "@dracula-network-offline-label" "Offline")"
   for host in $HOSTS; do
     if ping -q -c 1 -W 1 $host &>/dev/null; then
       network="$(get_ssid)"
